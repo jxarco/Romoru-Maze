410d2d23546a426d5eb6cb89bb2cb0b0a3bc8135
Alex <alexroco.30@gmail.com>
2017-02-13 20:30:54 +0100

    recosas

--- PTE/js/code.js
+++ PTE/js/code.js
@@ -44,66 +44,24 @@ var camera, scene, renderer;
 function example(){
 	
 	var geometry, material, mesh;
-	var controls;
 	var objects = [];
-	var raycaster;
-	var blocker = document.getElementById( 'blocker' );
-	var instructions = document.getElementById( 'instructions' );
-	// http://www.html5rocks.com/en/tutorials/pointerlock/intro/
-	var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
-	if ( havePointerLock ) {
-		var element = document.body;
-		var pointerlockchange = function ( event ) {
-			if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
-				controlsEnabled = true;
-				controls.enabled = true;
-				blocker.style.display = 'none';
-			} else {
-				controls.enabled = false;
-				blocker.style.display = '-webkit-box';
-				blocker.style.display = '-moz-box';
-				blocker.style.display = 'box';
-				instructions.style.display = '';
-			}
-		};
-		var pointerlockerror = function ( event ) {
-			instructions.style.display = '';
-		};
-		// Hook pointer lock state change events
-		document.addEventListener( 'pointerlockchange', pointerlockchange, false );
-		document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
-		document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
-		document.addEventListener( 'pointerlockerror', pointerlockerror, false );
-		document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
-		document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
-		instructions.addEventListener( 'click', function ( event ) {
-			instructions.style.display = 'none';
-			// Ask the browser to lock the pointer
-			element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
-			element.requestPointerLock();
-		}, false );
-	} else {
-		instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
-	}
+	
 	init();
 	animate();
-	var controlsEnabled = false;
 	var moveForward = false;
 	var moveBackward = false;
 	var moveLeft = false;
 	var moveRight = false;
 	var canJump = false;
 	var prevTime = performance.now();
-	var velocity = new THREE.Vector3();
 	function init() {
-		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+		camera = new THREE.PerspectiveCamera( 26, tam.width / tam.height, 1, 1000 );
+		camera.position.set(0, 5, 0);
 		scene = new THREE.Scene();
 		scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
 		var light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
 		light.position.set( 0.5, 1, 0.75 );
 		scene.add( light );
-		controls = new THREE.PointerLockControls( camera );
-		scene.add( controls.getObject() );
 		var onKeyDown = function ( event ) {
 			switch ( event.keyCode ) {
 				case 38: // up
@@ -151,7 +109,7 @@ function example(){
 		document.addEventListener( 'keyup', onKeyUp, false );
 		raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
 		// floor
-		geometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
+		geometry = new THREE.PlaneGeometry( 200, 200, 100, 100 );
 		geometry.rotateX( - Math.PI / 2 );
 		for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
 			var vertex = geometry.vertices[ i ];
@@ -172,8 +130,8 @@ function example(){
 		// objects
 
 		// WALLS
-		/*for(var i = 0; i < data.height; i++){
-			for(var j = 0; j < data.width; j++){
+		/*for(var i = 0; i < data.height / 2; i++){
+			for(var j = 0; j < data.width / 2; j++){
 				if(data.data[(i*myImage.width + j)*4] == 0){
 					var wallGeo = new THREE.BoxGeometry(1, 5, 1);
 					var wallMat = new THREE.MeshPhongMaterial( {
@@ -193,7 +151,6 @@ function example(){
 
 		// renderer
 		renderer = new THREE.WebGLRenderer();
-		renderer.setClearColor( 0xffffff );
 		renderer.setPixelRatio( window.devicePixelRatio );
 		renderer.setSize( tam.width, tam.height );
 		renderer.domElement.id = "my_canvas";
@@ -203,40 +160,16 @@ function example(){
 		window.addEventListener( 'resize', onWindowResize, false );
 	}
 	function onWindowResize() {
-		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.aspect = tam.width / tam.height;
 		camera.updateProjectionMatrix();
-		renderer.setSize( window.innerWidth, window.innerHeight );
+		renderer.setSize( tam.width, tam.height );
 	}
 	function animate() {
 		requestAnimationFrame( animate );
-		if ( controlsEnabled ) {
-			raycaster.ray.origin.copy( controls.getObject().position );
-			raycaster.ray.origin.y -= 10;
-			var intersections = raycaster.intersectObjects( objects );
-			var isOnObject = intersections.length > 0;
-			var time = performance.now();
-			var delta = ( time - prevTime ) / 1000;
-			velocity.x -= velocity.x * 10.0 * delta;
-			velocity.z -= velocity.z * 10.0 * delta;
-			velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-			if ( moveForward ) velocity.z -= 400.0 * delta;
-			if ( moveBackward ) velocity.z += 400.0 * delta;
-			if ( moveLeft ) velocity.x -= 400.0 * delta;
-			if ( moveRight ) velocity.x += 400.0 * delta;
-			if ( isOnObject === true ) {
-				velocity.y = Math.max( 0, velocity.y );
-				canJump = true;
-			}
-			controls.getObject().translateX( velocity.x * delta );
-			controls.getObject().translateY( velocity.y * delta );
-			controls.getObject().translateZ( velocity.z * delta );
-			if ( controls.getObject().position.y < 10 ) {
-				velocity.y = 0;
-				controls.getObject().position.y = 10;
-				canJump = true;
-			}
-			prevTime = time;
-		}
+		if(moveForward) camera.position.z -= 1;
+		if(moveBackward) camera.position.z += 1;
+		if(moveLeft) camera.position.x -= 1;
+		if(moveRight) camera.position.x += 1;
 		renderer.render( scene, camera );
 	}
 }
@@ -460,11 +393,5 @@ function popCube(argumentx, argumentz){
 
 		if(window.server_on) server.sendMessage(poppedPosition);
 	}
-}
-
-function movementLimits(x, z){
-	if(x > 60 || x < -60) return false;
-	if(z > 4.5 || z < -60) return false;
-	return true;
 }
 
